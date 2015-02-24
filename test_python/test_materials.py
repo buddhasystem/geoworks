d5df02c7210bb8ec9a6cd9028d08195362be9c69
buddhasystem <potekhin@bnl.gov>
2015-02-23 23:00:29 -0500

    Working test of XML parser (AGDD) for elements.

--- test_python/test_materials.py
+++ test_python/test_materials.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python
-
+import sys, getopt
 import gwMaterials
 from gwMaterials import *
 
-gmm = gwMaterialManager()
-gmm.parse_XML("../gwMaterials/StandardMaterials.xml")
-gmm.output_elements()
+
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv,"i:")
+    except getopt.GetoptError:
+        print 'test.py -i <filename>'
+        sys.exit(2)
+
+    if len(opts)==0:
+        print 'No input file specified, exiting...'
+        sys.exit(2)
+
+    xml_file = opts[0][1]
+
+    gmm = gwMaterialManager()
+
+    try:
+        gmm.parse_XML(xml_file)
+    except:
+        print 'Failed to parse file', xml_file
+        sys.exit(1)
+        
+    gmm.output_elements()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
